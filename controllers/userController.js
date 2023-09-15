7142d08914a25318ae5b1307a331bc108d51785e
Roshan <roshan.jha@hih7.in>
2023-09-15 11:03:28 +0530

    login controller and router added with authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/usersModel");
+
 exports.register = asyncHandler(async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
+});
+
+exports.login = asyncHandler(async (req, res) => {
+  if (req.body.name && req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.json(user);
+    } else {
+      res.json({ result: "No user found" });
+    }
+  } else {
+    res.json({ result: "No user found" });
+  }
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { register } = require("../controllers/userController");
-const { model } = require("mongoose");
+const { register, login } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
 
 module.exports = router;
