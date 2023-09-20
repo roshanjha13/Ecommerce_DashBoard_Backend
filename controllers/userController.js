0622cb3186b235238999436ec2b1e2adb101c66f
Roshan <roshan.jha@hih7.in>
2023-09-20 17:19:17 +0530

    add jwt authentication in backend on login and signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,35 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/usersModel");
+const Jwt = require("jsonwebtoken");
+const jwtKey = "e-comm";
 
 exports.register = asyncHandler(async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
   result = result.toObject();
   delete result.password;
-  res.send(result);
+  Jwt.sign({ result }, jwtKey, { expiresIn: "24h" }, (err, token) => {
+    if (err) {
+      res.send({
+        result: "something went wrong,Please try after sometimes",
+      });
+    }
+    res.send({ result, auth: token });
+  });
 });
 
 exports.login = asyncHandler(async (req, res) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      res.json(user);
+      Jwt.sign({ user }, jwtKey, { expiresIn: "24h" }, (err, token) => {
+        if (err) {
+          res.send({
+            result: "something went wrong,Please try after sometimes",
+          });
+        }
+        res.send({ user, auth: token });
+      });
     } else {
       res.json({ result: "No user found" });
     }
